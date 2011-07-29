d03de2b9d58086fd7731b9aed2717d045935f8c2
Drew Neil <andrew.jr.neil@gmail.com>
2011-07-29 11:04:49 +0100

    Make Viewport extend TabPanel with 5 placeholders.

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -1,3 +1,26 @@
-ToolbarDemo.views.Viewport = Ext.extend(Ext.Panel, {
-    fullscreen: true
+ToolbarDemo.views.Viewport = Ext.extend(Ext.TabPanel, {
+    fullscreen: true,
+
+    defaults: {
+        html: 'placeholder text',
+        styleHtmlContent: true
+    },
+
+    items: [
+        {
+            title: "Home"
+        },
+        {
+            title: "Search"
+        },
+        {
+            title: "Action"
+        },
+        {
+            title: "Settings"
+        },
+        {
+            title: "More"
+        },
+    ]
 });
