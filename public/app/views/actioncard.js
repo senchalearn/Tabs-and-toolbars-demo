d47efa2d922a7fee4d319e5d56f379a390dee9fc
Drew Neil <andrew.jr.neil@gmail.com>
2011-07-29 12:43:40 +0100

    Add toolbar to demonstrate icon buttons.

--- public/app/views/actioncard.js
+++ public/app/views/actioncard.js
@@ -58,6 +58,18 @@
         ]
     };
 
+    var imageButtons = {
+        xtype: 'toolbar',
+        dock: 'bottom',
+        items: [
+            { iconCls: 'star',  iconMask: true,  text: 'favorite' },
+            { iconCls: 'star',  iconMask: true,  ui: 'action' },
+            { iconCls: 'star',  iconMask: true,  ui: 'confirm' },
+            { iconCls: 'star',  iconMask: true,  ui: 'decline' },
+            { iconCls: 'star',  iconMask: true,  ui: 'plain' },
+        ]
+    };
+
     ToolbarDemo.views.Actioncard = Ext.extend(Ext.Panel, {
         title: "action",
         iconCls: "action",
@@ -67,7 +79,8 @@
             topbar,
             textButtonShapes,
             textButtonColors,
-            mixedTextButtons
+            mixedTextButtons,
+            imageButtons
         ]
     });
 
