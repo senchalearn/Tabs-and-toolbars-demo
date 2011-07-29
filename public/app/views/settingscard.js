4bb71d418fbffef24a176300f874873caae6626c
Drew Neil <andrew.jr.neil@gmail.com>
2011-07-29 11:32:42 +0100

    Customize the settings card by adding a form.

--- public/app/views/settingscard.js
+++ public/app/views/settingscard.js
@@ -1,6 +1,26 @@
-ToolbarDemo.views.Settingscard = Ext.extend(Ext.Panel, {
+ToolbarDemo.views.Settingscard = Ext.extend(Ext.form.FormPanel, {
     title: "settings",
-    iconCls: "settings"
+    iconCls: "settings",
+    scroll: "vertical",
+    items: [
+        {
+        xtype: 'fieldset',
+        title: 'Details',
+        items: [{
+            xtype: 'textfield',
+            name : 'name',
+            label: 'Username'
+            },{
+            xtype: 'passwordfield',
+            name : 'password',
+            label: 'Password'
+            }],
+        },{
+        xtype:  'button',
+        text:   'save',
+        ui:     'confirm'
+        }
+    ]
 });
 
 Ext.reg('settingscard', ToolbarDemo.views.Settingscard);
