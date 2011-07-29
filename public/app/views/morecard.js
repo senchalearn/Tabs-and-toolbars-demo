11982c63451af110a74d832b98ff52bae7792cc3
Drew Neil <andrew.jr.neil@gmail.com>
2011-07-29 11:39:47 +0100

    Customize the morecard with a nested list.

--- public/app/views/morecard.js
+++ public/app/views/morecard.js
@@ -1,6 +1,12 @@
-ToolbarDemo.views.Morecard = Ext.extend(Ext.Panel, {
-    title: "more",
-    iconCls: "more"
+ToolbarDemo.views.Morecard = Ext.extend(Ext.NestedList, {
+    title: "More",
+    iconCls: "more",
+    store: ToolbarDemo.morestore,
+    cardSwitchAnimation: 'slide',
+    getDetailCard: function(item, parent) {
+        var itemData = item.attributes.record.data;
+        return itemData.card;
+    }
 });
 
 Ext.reg('morecard', ToolbarDemo.views.Morecard);
